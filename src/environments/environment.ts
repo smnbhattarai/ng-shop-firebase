0487451cd1b51d81761dc5b6d44e41de9f9f1c14
suman <suman.bhattarai@gmail.com>
2020-05-13 22:00:08 +0545

    Initial setup - firebase-setup, components created, router defined, base layout created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA8kZrnktYaHd9eRckQvMyi2L_zNdApEpU",
+    authDomain: "oshop-8437d.firebaseapp.com",
+    databaseURL: "https://oshop-8437d.firebaseio.com",
+    projectId: "oshop-8437d",
+    storageBucket: "oshop-8437d.appspot.com",
+    messagingSenderId: "826270052674",
+    appId: "1:826270052674:web:412d760b00b5fac42158db"
+  }
 };
 
 /*
