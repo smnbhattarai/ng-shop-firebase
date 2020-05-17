775bd17aff1e735bbe14d023138070a6788dc93d
suman <suman.bhattarai@gmail.com>
2020-05-17 14:07:23 +0545

    Product admin CRUD done

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private productService: ProductService) { 
+    this.subscription = this.productService.getAll().subscribe(
+      products => {
+        this.filteredProducts = this.products = products;
+      }
+      );
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+    this.products.filter(p => p.payload.val().title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-form',
@@ -10,16 +12,40 @@ import { ProductService } from 'src/app/product.service';
 export class ProductFormComponent implements OnInit {
 
   categories$;
+  product = {};
+  id;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private categoryService: CategoryService, 
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
     this.categories$ = categoryService.getCategories();
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(this.id) {
+      this.productService.get(this.id).pipe(take(1)).subscribe(p => this.product = p.payload.val() );
+    }
    }
 
   ngOnInit() {
   }
 
   save(product) {
-    this.productService.create(product);
+    if(this.id) {
+      this.productService.update(this.id, product);
+    } else {
+      this.productService.create(product);
+    }
+    
+    this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if(confirm("Delete product?")) {
+      this.productService.delete(this.id);
+      this.router.navigate(['/admin/products']);
+    }
   }
 
 }
